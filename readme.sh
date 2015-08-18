1d361d09fb0b331626cd4064b9b9030255e987fb
jeff liu <liujinfeng98@hotmail.com>
2015-08-18 21:01:06 +0800

    test

--- readme.sh
+++ readme.sh
@@ -8,7 +8,13 @@ echo 'git init'
 echo 'git add readme.sh'
 echo 'git commit -m 提交git shell'
 #git add readme.sh
-#git commit -m '提交 readme.sh second'
+#
+#
+#
+#
+#
+#
+######git commit -m '提交 readme.sh second'
 git config --global color.ui true
 git status
 git diff readme.sh
